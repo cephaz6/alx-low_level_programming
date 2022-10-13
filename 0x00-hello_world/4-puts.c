9ab0e2dbe41b06aaab382b1efb6b578035218d4e
Oselumese Cephas Abainmun <cephasblog@gmail.com>
2022-10-13 12:40:02 -0700

    Learning the C-Language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
 
+
+/**
+ * main - The conventional entry point of a c program
+ * Return: When run completes, 0 is expected to be returned
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
