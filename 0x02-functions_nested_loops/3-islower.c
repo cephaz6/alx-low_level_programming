747fcd295c1f0089afc2aa258c32aee22ce9589e
Oselumese Cephas Abainmun <cephasblog@gmail.com>
2022-10-19 00:54:52 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,11 @@
 
 /**
  * _islower - checks if an alphabet is a lowercase
- *
- * Return: 0 when run successful
+ * c - as called by the function in the main.h file
+ * Return: 1 or 0 depending on the result
  */
 
 int _islower(int c)
 {
-	
 	return (c >= 'a' && c <= 'z');
 }
