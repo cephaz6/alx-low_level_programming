001bf417c878483704b884219cc18722778ea304
Oselumese Cephas Abainmun <cephasblog@gmail.com>
2022-10-19 00:21:34 -0700

    Learning Nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints the result of print_alphabetx10 ten times
+ * Return: 0 when run successful
+ */
+
+void  print_alphabet_x10(void)
+{
+	int alpha;
+	int col_filler;
+
+	for (col_filler = 0; col_filler < 10; col_filler++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
