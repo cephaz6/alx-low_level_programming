0df73e30ee723d2e1a74ece21f4108937fe3461d
Oselumese Cephas Abainmun <cephasblog@gmail.com>
2022-10-19 10:57:29 -0700

    function to print the last digit of a number group

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number group
+ * @r: integer argument passed
+ * Return: return an integer when successful
+ */
+
+
+int print_last_digit(int r)
+{
+	int lastnum = r % 10;
+
+	if (n < 0)
+	{
+		lastnum = lastnum * -1;
+		_putchar(lastnum + '0');
+	}
+	return (lastnum);
+}
