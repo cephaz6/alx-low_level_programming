d2c4522073d58ee090a543af74a1da5f4080a22d
Oselumese Cephas Abainmun <cephasblog@gmail.com>
2022-10-19 00:51:28 -0700

    Function to check if an alphabet is a lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - checks if an alphabet is a lowercase
+ *
+ * Return: 0 when run successful
+ */
+
+int _islower(int c)
+{
+	
+	return (c >= 'a' && c <= 'z');
+}
