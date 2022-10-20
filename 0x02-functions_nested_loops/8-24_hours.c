0bbfea180171c2b828bac94f3045b32b899c4484
Oselumese Cephas Abainmun <cephasblog@gmail.com>
2022-10-19 18:56:00 -0700

    Function that prints time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function that prints every minute of the day
+ * Return: returns 0 on successful 
+ */
+
+void jack_bauer(void)
+{
+	int x;
+	int y;
+	for (x = 0; x < 24; x++)
+	{
+		for (y = 0; y < 60; y++)
+		{
+			if (x < 10)
+			{
+				_putchar('0');
+				_putchar(x + '0');
+			}
+			else if (x >= 10)
+			{
+				_putchar((x/10) + ''0');
+				_putchar((x % 10) + '0');
+			}
+			if (j < 10)
+			{
+				_putchar(':');
+				_putchar('0');
+				_putchar(y);
+			}
+			else if (y >= 10)
+			{
+				_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
+			}
+			_putchar(x);
+			_putchar(':');
+			_putchar(y);
+		}
+	}
+}
