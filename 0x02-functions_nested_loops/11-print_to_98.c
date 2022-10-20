6d3a22ac59e72267aabfd016bcbd2c1935e7fcc4
Oselumese Cephas Abainmun <cephasblog@gmail.com>
2022-10-19 19:49:10 -0700

    function that prints to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * void print_to_98 - function that prints to 98
+ * @n: integer argument passed
+ * Return: return 0 on completion
+ */
+
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("98\n");
+}
