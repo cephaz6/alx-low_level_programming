f3a0c6618f31df1dffed36e3898ea43394af8378
Oselumese Cephas Abainmun <cephasblog@gmail.com>
2022-10-19 21:55:56 -0700

    final fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,10 +11,10 @@
 int main(void)
 {
 	unsigned long int i, j, k, j1, j2, k1, k2;
+	
 	j = 1;
 	k = 2;
 	printf("%lu", j);
-	
 	for (i = 1; i < 91; i++)
 	{
 		printf(", %lu", k);
@@ -25,7 +25,6 @@ int main(void)
 	j2 = j % 1000000000;
 	k1 = k / 1000000000;
 	k2 = k % 1000000000;
-	
 	for (i = 92; i < 99; ++i)
 	{
 		printf(", %lu", k1 + (k2 / 1000000000));
