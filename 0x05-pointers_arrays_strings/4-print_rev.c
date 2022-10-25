1c8fcafdebe364fe485c627beed7d426f1cdd131
Oselumese Cephas Abainmun <cephasblog@gmail.com>
2022-10-25 05:00:34 -0700

    function to print string inreverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * print_rev - prints strings in reverse
+ * @str: string
+ * Return: void
+ */
+
+void print_rev(char *str)
+{
+	int i = 0;
+	
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		_putchar(str[i]);
+		i--;
+	}
+	_putchar('\n');
+}
